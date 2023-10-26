5a5d5c0aefd12e4335da494dfbff28a897fafc3a
Yuvasri3010 <yuvasri6674@gmail.com>
2023-10-26 21:12:08 +0530

    first commit

---
+++ res.js
@@ -0,0 +1,42 @@
+const myresume = {
+  name : "Nancy",
+  age : "25+",
+  place : "chennai",
+  Qualification : [
+    {
+      Level : "PG",
+      Dept : "IT",
+      College : "SRM College",
+      Percentage : "78"
+    },
+      
+    {
+    Level : "UG",
+      Dept : "IT",
+      College : "Pachayapas College",
+      Percentage : "98"
+  },
+  {
+    Level : "HSC",
+      Dept : "Bio Maths",
+      College : "Don Bosco",
+      Percentage : "78"
+
+  },
+  {
+    Level : "SSLC",
+      Dept : "Science",
+      College : "Sunbeam",
+      Percentage : "99"
+  },
+  ]
+Languageknown :["Tamil","English"],
+ProgrammingLanguage : [
+  "C"
+  "C++"
+  "Java"
+  "Python"
+  "Javascript"
+]
+
+console.log(myresume);
